94eec9095553c8ee9eec897ed1e54a41908c50c8
wafel001 <89450053+wafel001@users.noreply.github.com>
2026-08-13 14:09:34 +0200

    Add TriCoach v2.2 boot receiver

---
+++ v22/app/src/main/java/pl/tricoach/mobile/BootReceiver.java
@@ -0,0 +1,11 @@
+package pl.tricoach.mobile;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public final class BootReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {
+        JobSchedulerHelper.schedule(context);
+    }
+}
